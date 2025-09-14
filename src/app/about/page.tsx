1594c5bdde3d5fd168204b36ef137124f92f26ce
Rafat Alam <Rafat.Alam.RA@gmail.com>
2025-09-14 11:16:11 +0530

    Completed v0.0.2

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { AboutPage } from '@/components/about-page'
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+      <AboutPage />
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
-import { HomePage1 } from '@/components/home-page-1'
-import HomePage2 from '@/components/home-page-2'
-import HomePage3 from '@/components/home-page-3'
-import HomePage4 from '@/components/home-page-4'
-import HomePage5 from '@/components/home-page-5'
-import HomePage6 from '@/components/home-page-6'
+import { HomePage } from '@/components/home-page'
 import React from 'react'
 
 export default function Home() {
   return (
     <>
-      <HomePage1 />
-      <HomePage2 />
-      <HomePage3 />
-      <HomePage4 />
-      <HomePage5 />
-      <HomePage6 />
+      <HomePage />
     </>
   )
 }
